9a5dfc12d2b76f49b617981cb4bd5a5f30ad16cc
Ana Caroline Pontara | BT Créditos <apontara@btcreditos.com.br>
2023-05-12 14:46:19 -0300

    Variaveis, int e double

---
+++ 3.VariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario;
+        salario = 3000.10;
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle qualquer tecla para fechar ...");
+        Console.ReadLine();
+    }
+}
